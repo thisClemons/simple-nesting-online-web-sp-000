660fa66eaf2b482624b2512173688a3fdff33dbb
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-22 19:18:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,9 +39,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
-programmer_hash[:alan_kay][:known_for]
 
 def dennis_ritchies_language
 	programmer_hash =
